079e096fe90bf54875a3c9e1512dcb61879156e5
Ganesh Natesan <ganeshnatesan.work@gmail.com>
2020-12-15 15:24:10 +0530

    First Commit
    I am begining to learn python with github

---
+++ learn_01/test03.py
@@ -0,0 +1,9 @@
+# a, b,c = 10, 20, "its a string.."
+# print (a+b)
+# print("Hello "+str(a)+"\n")
+# print("Hello {}".format (a))
+a,b=10, 0
+if a+b>=10:
+	print("Sum is greater than 10")
+else:
+	print("It's too small a number")
